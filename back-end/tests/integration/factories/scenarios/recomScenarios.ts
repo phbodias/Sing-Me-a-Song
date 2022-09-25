5a032b71932685856e1bf78cedfc4c48f311446e
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-09-25 20:39:35 -0300

    integration tests for get recommendations

--- back-end/tests/integration/factories/scenarios/recomScenarios.ts
+++ back-end/tests/integration/factories/scenarios/recomScenarios.ts
@@ -1,15 +1,43 @@
 import { prisma } from "../../../../src/database";
-import recommendationFac from "../createRecomFacs/rightRecomFactory";
+import { Recommendation as IRecommendationData } from "@prisma/client";
+import {
+  recommendationFac,
+  recommendationFacWithScore,
+} from "../createRecomFacs/rightRecomFactory";
 
-export async function recomToVote() {
+export type TRecomBody = Omit<IRecommendationData, "id" | "score">;
+export type TRecomAmount = Omit<IRecommendationData, "id">;
+
+export async function insertRecom(): Promise<IRecommendationData> {
   return await prisma.recommendation.create({
     data: await recommendationFac(),
   });
 }
 
-export async function recomToWillBeDeleted() {
-  const body = await recommendationFac();
+export async function recomToWillBeDeleted(): Promise<IRecommendationData> {
+  const body: TRecomBody = await recommendationFac();
   return await prisma.recommendation.create({
     data: { ...body, score: -5 },
   });
+}
+
+export async function createManyRecoms() {
+  const recoms = [];
+  while (recoms.length < 12) {
+    const recom: TRecomBody = await recommendationFac();
+    const repeatedName = recoms.find((element) => element.name === recom.name);
+    if (!repeatedName) recoms.push(recom);
+  }
+  return await prisma.recommendation.createMany({ data: recoms });
+}
+
+export async function createRecomsWithScores(){
+  const recoms = [];
+  while (recoms.length < 10) {
+    const recom: TRecomAmount = await recommendationFacWithScore();
+    const repeatedName = recoms.find((element) => element.name === recom.name);
+    if (!repeatedName) recoms.push(recom);
+  }
+
+  return await prisma.recommendation.createMany({data: recoms});
 }
--- back-end/tests/integration/recommendations.test.ts
+++ back-end/tests/integration/recommendations.test.ts
@@ -1,9 +1,17 @@
+import { faker } from "@faker-js/faker";
 import { prisma } from "../../src/database";
+import { Recommendation as IRecommendationData } from "@prisma/client";
 import supertest from "supertest";
 import app from "../../src/app";
-import recommendationFac from "./factories/createRecomFacs/rightRecomFactory";
+import { recommendationFac } from "./factories/createRecomFacs/rightRecomFactory";
 import wrongDomainRecomFac from "./factories/createRecomFacs/wrongRecomFactorie";
-import { recomToVote, recomToWillBeDeleted } from "./factories/scenarios/recomScenarios";
+import {
+  createManyRecoms,
+  createRecomsWithScores,
+  insertRecom,
+  recomToWillBeDeleted,
+  TRecomBody,
+} from "./factories/scenarios/recomScenarios";
 
 beforeEach(async () => {
   await prisma.$executeRaw`TRUNCATE TABLE recommendations RESTART IDENTITY CASCADE;`;
@@ -14,30 +22,33 @@ afterAll(async () => {
 });
 
 describe("tests for POST /recommendations", () => {
-  it("try to insert a new recommendation with a right body and received status 201", async () => {
-    const newRecom = await recommendationFac();
+  it("try to insert a new recommendation with a right body and receives status 201", async () => {
+    const newRecom: TRecomBody = await recommendationFac();
 
     const { status } = await supertest(app)
       .post("/recommendations")
       .send(newRecom);
-    const recomInDB = await prisma.recommendation.findUnique({
-      where: { name: newRecom.name },
-    });
+    const recomInDB: IRecommendationData =
+      await prisma.recommendation.findUnique({
+        where: { name: newRecom.name },
+      });
 
     expect(status).toBe(201);
     expect(recomInDB).not.toBeFalsy();
   });
 
-  it("try to insert a new recommendation with a wrong body (domain isn't a 'youtube' or 'youtu' link) and received status 422", async () => {
-    const newRecom = await wrongDomainRecomFac();
+  it("try to insert a new recommendation with a wrong body (domain isn't a 'youtube' or 'youtu' link) and receives status 422", async () => {
+    const newRecom: TRecomBody = await wrongDomainRecomFac();
 
-    const result = await supertest(app).post("/recommendations").send(newRecom);
+    const { status } = await supertest(app)
+      .post("/recommendations")
+      .send(newRecom);
 
-    expect(result.status).toBe(422);
+    expect(status).toBe(422);
   });
 
-  it("try to insert a new recommendation with a name who already exists in DB and received status 409", async () => {
-    const recom = await recommendationFac();
+  it("try to insert a new recommendation with a name who already exists in DB and receives status 409", async () => {
+    const recom: TRecomBody = await recommendationFac();
 
     await prisma.recommendation.create({ data: recom });
     const { status } = await supertest(app)
@@ -49,8 +60,8 @@ describe("tests for POST /recommendations", () => {
 });
 
 describe("tests for POST /recommendations/:id/upvote", () => {
-  it("upvote a existing recommendation and received status 200", async () => {
-    const { id, score } = await recomToVote();
+  it("upvote a existing recommendation and receives status 200", async () => {
+    const { id, score } = await insertRecom();
 
     const { status } = await supertest(app).post(
       `/recommendations/${id}/upvote`
@@ -59,16 +70,16 @@ describe("tests for POST /recommendations/:id/upvote", () => {
       where: { id },
     });
 
-    const upvoteHappened: boolean = scoreUpvoted > score;
+    const upvoteHappened: boolean = scoreUpvoted === score + 1;
 
     expect(status).toBe(200);
     expect(upvoteHappened).not.toBeFalsy();
   });
 
-  it("try to upvote a recommendation with a invalid id and received status 404", async () => {
-    const { id: lastRecomId } = await recomToVote(); //insert a new recommendation to get a last recommendation_id in DB
+  it("try to upvote a recommendation with a invalid id and receives status 404", async () => {
+    const { id: lastRecomId } = await insertRecom(); //insert a new recommendation to get a last recommendation_id in DB
 
-    const invalidId = lastRecomId + 1;
+    const invalidId: Number = lastRecomId + 1;
 
     const { status } = await supertest(app).post(
       `/recommendations/${invalidId}/upvote`
@@ -79,8 +90,8 @@ describe("tests for POST /recommendations/:id/upvote", () => {
 });
 
 describe("tests for POST /recommendations/:id/downvote", () => {
-  it("Downvote a existing recommendation and received status 200", async () => {
-    const { id, score } = await recomToVote();
+  it("Downvote a existing recommendation and receives status 200", async () => {
+    const { id, score } = await insertRecom();
 
     const { status } = await supertest(app).post(
       `/recommendations/${id}/downvote`
@@ -89,16 +100,16 @@ describe("tests for POST /recommendations/:id/downvote", () => {
       where: { id },
     });
 
-    const upvoteHappened: boolean = scoreDownVoted < score;
+    const upvoteHappened: boolean = scoreDownVoted === score - 1;
 
     expect(status).toBe(200);
     expect(upvoteHappened).not.toBeFalsy();
   });
 
-  it("try to downvote a recommendation with a invalid id and received status 404", async () => {
-    const { id: lastRecomId } = await recomToVote(); //insert a new recommendation to get a last recommendation_id in DB
+  it("try to downvote a recommendation with a invalid id and receives status 404", async () => {
+    const { id: lastRecomId } = await insertRecom(); //insert a new recommendation to get a last recommendation_id in DB
 
-    const invalidId = lastRecomId + 1;
+    const invalidId: Number = lastRecomId + 1;
 
     const { status } = await supertest(app).post(
       `/recommendations/${invalidId}/downvote`
@@ -107,13 +118,104 @@ describe("tests for POST /recommendations/:id/downvote", () => {
     expect(status).toBe(404);
   });
 
-  it("let a recommendation with a score < -5, checks if then it will be deleted and received status 200", async() => {
+  it("let a recommendation with a score < -5, checks if then it will be deleted and receives status 200", async () => {
     const { id, name } = await recomToWillBeDeleted();
 
-    const { status } = await supertest(app).post(`/recommendations/${id}/downvote`);
-    const recom = await prisma.recommendation.findUnique({where: {name}});
+    const { status } = await supertest(app).post(
+      `/recommendations/${id}/downvote`
+    );
+    const recom: IRecommendationData = await prisma.recommendation.findUnique({
+      where: { name },
+    });
 
     expect(status).toBe(200);
     expect(recom).toBeFalsy();
-  })
+  });
+});
+
+describe("tests for GET /recommendations", () => {
+  it("get the 10 last recommendations and receives status 200", async () => {
+    await createManyRecoms();
+
+    const recommsInDB: IRecommendationData[] = (
+      await prisma.recommendation.findMany()
+    )
+      .slice(2, 12)
+      .reverse();
+
+    const { status, body } = await supertest(app).get("/recommendations");
+
+    const verifyBody: boolean =
+      body.filter((elem: IRecommendationData, index: any) => {
+        return elem.id === recommsInDB[index].id;
+      }).length === recommsInDB.length;
+
+    expect(status).toBe(200);
+    expect(verifyBody).not.toBeFalsy();
+  });
+});
+
+describe("tests for GET /recommendations/:id", () => {
+  it("get a recommendation by recommendation_id and receives status 200 and the recommendation", async () => {
+    const { id, name } = await insertRecom();
+
+    const { status, body } = await supertest(app).get(`/recommendations/${id}`);
+
+    const result: boolean = body.name === name;
+
+    expect(status).toBe(200);
+    expect(result).not.toBeFalsy();
+  });
+
+  it("insert a invalid recommendation_id and receives status 404", async () => {
+    const { id: lastRecomId } = await insertRecom();
+
+    const invalidId: number = lastRecomId + 1;
+
+    const { status } = await supertest(app).get(
+      `/recommendations/${invalidId}`
+    );
+
+    expect(status).toBe(404);
+  });
+});
+
+describe("tests for GET /recommendations/random", () => {
+  it("get a random recommendation and receives status 200", async () => {
+    await createManyRecoms();
+
+    const { status, body } = await supertest(app).get(
+      "/recommendations/random"
+    );
+
+    expect(status).toBe(200);
+    expect(body).not.toBeFalsy();
+  });
+
+  it("receives status 404 when no recommendations exist", async () => {
+    const result = await supertest(app).get("/recommendations/random");
+
+    expect(result.status).toBe(404);
+  });
+});
+
+describe("tests for GET /recommendations/top/:amount", () => {
+  it("get a x recommendations (where x = amount) and receives status 200", async () => {
+    await createRecomsWithScores();
+
+    const amount = faker.datatype.number({ min: 1, max: 10 });
+
+    const { status, body } = await supertest(app).get(
+      `/recommendations/top/${amount}`
+    );
+
+    const result: boolean =
+      body.filter((recom: IRecommendationData, i: any) => {
+        if (i < body.length - 1) return recom.score >= body[i + 1].score;
+        return recom;
+      }).length === body.length;
+
+    expect(status).toBe(200);
+    expect(result).not.toBeFalsy();
+  });
 });
